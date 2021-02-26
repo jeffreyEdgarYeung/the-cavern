7c228a255d01df13828dcedb14cf543a4286f42f
Jeffrey Yeung <jeffrey.edgar.yeung@gmail.com>
2021-02-26 14:52:00 -0800

    Rough jumping implementation

--- Assets/Sprites & Tiles/Scripts/Player.cs
+++ Assets/Sprites & Tiles/Scripts/Player.cs
@@ -7,12 +7,26 @@ public class Player : MonoBehaviour
     [Header("Player Status")]
     [SerializeField] [Range(0, 50)] float maxSpeed = 10f;
 
+    [Header("Jump Parameters")]
+    [SerializeField] float jumpHeight = 1f;
+    [SerializeField] bool isGrounded;
+    [SerializeField] bool isJumping;
+    [SerializeField] bool jumpKeyHeld;
+    [SerializeField] Vector2 counterJumpForce;
+    [SerializeField] private LayerMask platformLayerMask;
+
+    // Player State
+    float jumpForce;
+
     Rigidbody2D rigidBody;
+    CapsuleCollider2D capsuleCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        jumpForce = Mathf.Sqrt(2 * Physics2D.gravity.magnitude * jumpHeight);
     }
 
     // Update is called once per frame
@@ -20,10 +34,19 @@ public class Player : MonoBehaviour
     {
         Move();
         HandleSpriteDirection();
+        isGrounded = IsGrounded();
+        isJumping = !isGrounded;
     }
+
+    void FixedUpdate()
+    {
+        HandlePartialJump();
+    }
+
     private void Move()
     {
         Run();
+        HandleJump();
     }
 
     private void Run()
@@ -49,4 +72,57 @@ public class Player : MonoBehaviour
             GetComponent<Animator>().SetBool("isRunning", false);
         }
     }
+
+    private void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpKeyHeld = true;
+
+            if (isGrounded)
+            {
+                isJumping = true;
+                rigidBody.AddForce(Vector2.up * jumpForce * rigidBody.mass, ForceMode2D.Impulse);
+            }
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            jumpKeyHeld = false;
+        }
+    }
+
+    private void HandlePartialJump()
+    {
+        if (isJumping)
+        {
+            if(!jumpKeyHeld && Vector2.Dot(rigidBody.velocity, Vector2.up) > 0)
+            {
+                rigidBody.AddForce(counterJumpForce * rigidBody.mass);
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float extraHeightText = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0f, Vector2.down, extraHeightText, platformLayerMask);
+
+        Color rayColor;
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(capsuleCollider.bounds.center + new Vector3(capsuleCollider.bounds.extents.x, 0), Vector2.down * (capsuleCollider.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(capsuleCollider.bounds.center - new Vector3(capsuleCollider.bounds.extents.x, 0), Vector2.down * (capsuleCollider.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(capsuleCollider.bounds.center - new Vector3(capsuleCollider.bounds.extents.x, capsuleCollider.bounds.extents.y + extraHeightText), Vector2.right * (capsuleCollider.bounds.extents.x * 2f), rayColor);
+
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
+    
+   
 }
